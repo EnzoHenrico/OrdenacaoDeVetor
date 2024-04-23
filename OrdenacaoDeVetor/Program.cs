88640fb4492fc7196667c0d4310a32ac6ef264a1
EnzoHenrico <enzo.mascia1@gmail.com>
2024-04-23 13:48:52 -0300

    implemented bubble sort

--- OrdenacaoDeVetor/Program.cs
+++ OrdenacaoDeVetor/Program.cs
@@ -1,39 +1,31 @@
 ﻿int[] numbers = new int[5], sorted = new int[5];
-int indexOfMinimum = 0, temp = 0;
+int temp = 0, size = 5;
 
 Console.WriteLine("Crie um vetor para ser ordenado: \n");
 
-for (int i = 0; i < 5; i++)
+for (int i = 0; i < size; i++)
 {
     Console.Write($"Numero {i + 1}: ");
     numbers[i] = int.Parse(Console.ReadLine());
+    sorted[i] = numbers[i];
 }
 
-sorted = numbers;
-
-// Ordenação utilizando selection sort
-for (int i = 0; i < 5; i++)
+// Ordenação utilizando bubble sort
+for (int i = 0; i < size - 1; i++)
 {
-    // Procurar o menor relativo ao primeiro item não ordenado   
-    indexOfMinimum = i;
-
-    // Iterar sob todos elementos procurando o index do número de menor valor
-    for (int j = i; j < 5; j++)
+    for (int j = i + 1; j < size; j++)
     {
-        if (sorted[j] < sorted[indexOfMinimum])
+        if (sorted[i] > sorted[j])
         {
-            indexOfMinimum = j;
+            temp = sorted[i];
+            sorted[i] = sorted[j];
+            sorted[j] = temp;
         }
     }
-    // Trocar o item do index atual com o de menor valor
-    temp = sorted[i];
-    sorted[i] = sorted[indexOfMinimum];
-    sorted[indexOfMinimum] = temp;
-    
 }
 
 Console.Write("\n\nVetor ordenado:");
-for (int i = 0; i < 5; i++)
+for (int i = 0; i < size; i++)
 {
     Console.Write(" " + sorted[i]);
 }
